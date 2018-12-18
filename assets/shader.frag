3a0e2857a55bcf2b18634e2c7e4b32df323d8a3d
Shirley Wu <shirleyxueyangwu@gmail.com>
2018-12-18 15:16:06 -0800

    Use spheregeometry and gradient shader

--- assets/shader.frag
+++ assets/shader.frag
@@ -1,15 +1,22 @@
-uniform float color;
+uniform float colorType;
 varying vec2 vUv;
 // for noise
 #pragma glslify: noise = require('glsl-noise/simplex/3d');
 #pragma glslify: hsl2rgb = require('glsl-hsl2rgb');
 
+#define PI 3.14159265359
+vec3 colorA = vec3(0.149,0.141,0.912);
+vec3 colorB = vec3(1.000,0.833,0.224);
+
 void main() {
-  float n = noise(vec3(vUv, 0.25));
-  vec3 rgb = hsl2rgb(
-    color,
-    0.75 - n * 0.1,
-    0.75 - n * 0.25
-  );
-  gl_FragColor = vec4(rgb, 1.0);
+  vec3 color = vec3(0.0);
+
+  vec3 pct = vec3(vUv, 1.0);
+  
+  pct.r = 1.0;
+  pct.g = sin(vUv.y) * 1.5;
+  pct.b = sin(vUv.y * 1.0);
+
+  color = mix(colorA, colorB, pct);
+  gl_FragColor = vec4(color, 1.0);
 }
--- components/World.vue
+++ components/World.vue
@@ -4,7 +4,7 @@
 
 <script>
 import _ from 'lodash'
-import {extent, scaleLinear, scaleQuantize} from 'd3'
+import {extent, scaleLinear, scaleOrdinal, scaleQuantize} from 'd3'
 import * as THREE from 'three'
 const OrbitControls = require('three-orbit-controls')(THREE)
 const vertexShader = require('../assets/shader.vert')
@@ -38,6 +38,16 @@ export default {
     const controls = new OrbitControls(this.camera, this.renderer.domElement);
     controls.addEventListener("change", () => this.renderer.render(this.scene, this.camera));
 
+    // lights
+    const ambientLight = new THREE.AmbientLight(0x666666);
+		const directionalLight = new THREE.DirectionalLight(0xffbbaa);
+
+		directionalLight.position.set(0, 1, 1);
+		// directionalLight.target.position.copy(this.scene.position);
+
+		this.scene.add(directionalLight);
+		this.scene.add(ambientLight);
+
     // create scales:
     // faces: number of sources
     // z-index: decade of award
@@ -45,11 +55,13 @@ export default {
     const facesDomain = extent(this.legends, d => d.references)
     const sizeDomain = extent(this.legends, d => d.backlinks)
     const zDomain = extent(this.legends, d => d.decade)
-    this.facesScale = scaleQuantize().domain(facesDomain).range([1, 2, 3])
-    this.sizeScale = scaleLinear().domain(sizeDomain).range([0.25, 2])
+    const colorsDomain = _.chain(this.legends).map('category').uniq().value()
+    this.facesScale = scaleQuantize().domain(facesDomain).range(_.range(4, 7))
+    this.sizeScale = scaleLinear().domain(sizeDomain).range([0.5, 2])
     this.xScale = scaleLinear().domain([0, 9]).range([-5, 5])
-    this.zScale = scaleLinear().domain(zDomain).range([-20, 0])
-    this.colors = _.chain(this.legends).map('category').uniq().value()
+    this.zScale = scaleLinear().domain(zDomain).range([-50, 0])
+    this.colorScale = scaleOrdinal().domain(colorsDomain)
+      .range([0xfd3243, 0xc4eb10, 0x00ed62, 0x00dcfa, 0x7e65ff, 0xfd7fcd])
   },
   mounted() {
     this.$refs.container.appendChild(this.renderer.domElement)
@@ -64,74 +76,37 @@ export default {
         const size = this.sizeScale(d.backlinks)
         const x = this.xScale(d.year - d.decade)
         const z = this.zScale(d.decade)
-        const color = this.colors.indexOf(d.category) / this.colors.length
+        const color = this.colorScale(d.category)
 
         const mesh = this.createMesh(faces, color)
         mesh.scale.set(size * 0.5, size, size * 0.5)
-        mesh.position.set(x, 0, z)
+        // mesh.scale.set(size, size, size)
+        mesh.position.set(x, -size / 2, z)
 
         this.scene.add(mesh)
       })
     },
     createMesh: function(numFaces, color) {
-      // create just one triangle
-      let vertices = [
-        -1, 1, 1, // 0: left top front
-        -1, -1, 1, // 1: left bottom front
-        1, -1, 1, // 2: right bottom front
-        1, 1, 1, // 3: right top front
-        1, -1, -1, // 4: right bottom back
-        1, 1, -1, // 5: right top back
-        -1, -1, -1, // 6: left bottom back
-        -1, 1, -1, // 7: left top back
-      ]
-      let faces = [
-        0, 1, 2, // front 1
-        0, 2, 3, // front 2
-        3, 2, 4, // right 1
-        3, 4, 5, // right 2
-        5, 4, 6, // back 1
-        5, 6, 7, // back 2
-        7, 6, 1, // left 1
-        7, 1, 0, // left 2
-        0, 3, 7, // top 1
-        3, 5, 7, // top 2
-        1, 2, 6, // bottom 1
-        2, 4, 6, // bottom 2
-      ]
-      if (numFaces > 1) {
-        // add top pyramid
-        vertices = _.concat(vertices, [
-          0, 1, 0, // 8: top middle
-        ])
-        faces = _.concat(faces, [
-          8, 0, 3, // top front
-          8, 3, 5, // top right
-          8, 5, 7, // top back
-          8, 7, 0, // top left
-        ])
-      }
-      if (numFaces > 2) {
-        vertices = _.concat(vertices, [
-          0, -1, 0, // 9: bottom middle
-        ])
-        faces = _.concat(faces, [
-          9, 2, 1, // bottom front
-          9, 4, 2, // bottom right
-          9, 6, 4, // bottom back
-          9, 1, 6, // bottom left
-        ])
-      }
-
-      const geometry = new THREE.PolyhedronGeometry(vertices, faces, 1, 0)
+      const geometry = new THREE.SphereGeometry( 1, numFaces, 4 )
+      // const material = new THREE.MeshPhongMaterial({
+			// 	color: color,
+			// 	flatShading: true
+			// })
       const material = new THREE.ShaderMaterial({
         flatShading: true,
         vertexShader,
         fragmentShader,
         side: THREE.DoubleSide,
         uniforms: {
-          color: {value: color},
+          colorType: {value: color / 0xffffff},
         },
+      })
+
+      // jitter vertices
+      _.each(geometry.vertices, v => {
+        v.x += _.random(-0.1, 0.1)
+        v.y += _.random(-0.1, 0.1)
+        v.z += _.random(-0.1, 0.1)
       })
       return new THREE.Mesh(geometry, material)
     }
